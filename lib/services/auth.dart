ec9cd670c62387127a75ebaaa48b6c58053e6dad
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-06 17:49:12 +0200

    Change all class to comments to test the other

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,22 +1,22 @@
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
 
 
-class Auth {
-  final _auth = FirebaseAuth.instance;
+// class Auth {
+//   final _auth = FirebaseAuth.instance;
 
- // get user => null;
+//  // get user => null;
 
-  Future signUp(String email, String password) async {
-    final  authResult = await _auth.createUserWithEmailAndPassword(
-        email: email, password: password);
+//   Future signUp(String email, String password) async {
+//     final  authResult = await _auth.createUserWithEmailAndPassword(
+//         email: email, password: password);
         
-    return authResult;
-  }
+//     return authResult;
+//   }
 
-  Future signIn(String email, String password) async {
-    final authResult = await _auth.signInWithEmailAndPassword(
-        email: email, password: password);
-    return authResult;
-  }
-}
+//   Future signIn(String email, String password) async {
+//     final authResult = await _auth.signInWithEmailAndPassword(
+//         email: email, password: password);
+//     return authResult;
+//   }
+// }
