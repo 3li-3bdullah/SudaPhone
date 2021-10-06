1b66ae99e5313110cf3b6e1b330fb633b7c410e3
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-06 18:13:26 +0200

    Added some function

--- lib/compount/validinput.dart
+++ lib/compount/validinput.dart
@@ -1,1 +1,16 @@
 import 'package:flutter/material.dart';
+
+valiInput(String val, int max, int min) {
+  max = 25;
+  min = 4;
+  if (val.trim().isEmpty) {
+    return "The field is empty";
+  }
+  if (val.length > max) {
+    return "The ${type} shouldn't to be more than ${max} char";
+  }
+  if (val.length < min) {
+    return "The ${type} shouldn't to be less than ${min} char";
+  }
+  
+}
