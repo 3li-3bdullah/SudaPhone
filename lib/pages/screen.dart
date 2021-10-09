86bd21a0b8833afe9cf056fe3cb4457c1a53a1aa
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-09 22:16:21 +0200

    added buildcategorieslogo package

--- lib/pages/screen.dart
+++ lib/pages/screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import '../widgets/build_categories_logo.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import '../compount/mydrawer.dart';
 import 'dart:core';
@@ -314,32 +315,7 @@ class ScreenState extends State<Screen> {
   }
 }
 
-class BuildCategoriesLogo extends StatelessWidget {
-  const BuildCategoriesLogo({
-     this.imageLogo, this.text, this.onTap,
-  });
-  final String imageLogo;
-  final String text;
-  final Function onTap;
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      child: Container(
-          height: 100,
-          width: 100,
-          child: ListTile(
-              title: Image.asset(
-                imageLogo,
-                height: 80,
-                width: 80,
-                fit: BoxFit.cover,
-              ),
-              subtitle: Container(
-                  child: Text(text, textAlign: TextAlign.center)))),
-      onTap: onTap,
-    );
-  }
-}
+
 
 class DataSearch extends SearchDelegate<String> {
   @override
