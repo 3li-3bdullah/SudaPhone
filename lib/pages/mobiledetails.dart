8208e6728e4041537f91cc0912e5d15fbf8b9273
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-10 18:35:21 +0200

    Renamed the class name and all function

--- lib/pages/mobiledetails.dart
+++ lib/pages/mobiledetails.dart
@@ -72,37 +72,37 @@ class _MobileDetailsState extends State<MobileDetails> {
                 padding: EdgeInsets.all(10),
                 child: Column(
                   children: <Widget>[
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1: "الموديل : ",
                       text2: "S20 Ultra",
                       containerColor: kcontainerColor1,
                     ),
-                     TextOfDetails(
+                     CustomTextOfDetails(
                       text1:  "الشاشة : ",
                       text2:  "Super Amoled 5.5",
                       containerColor: kcontainerColor2,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:  "نظام التشغيل : ",
                       text2:  "Android 10, One UI 2",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:  "الحماية  : ",
                       text2:   " IP68 مقاوم للماء , Gorilla Glass 6",
                       containerColor: kcontainerColor2,
                     ),
-                   TextOfDetails(
+                   CustomTextOfDetails(
                       text1:  "دقة الشاشة : ",
                       text2:   "6.9 بوصة بدقة QHD + بها ثقب صغير",
                       containerColor: kcontainerColor1,
                     ),
-                   TextOfDetails(
+                   CustomTextOfDetails(
                       text1:  "المعالج : ",
                       text2:   "Snapdragon 865, Eiyons 990",
                       containerColor: kcontainerColor2,
                     ),
-                  TextOfDetails(
+                  CustomTextOfDetails(
                       text1: "البطارية : ",
                       text2: "5000 MAH " + "" + "شحن سريع w15",
                       containerColor: kcontainerColor1,
@@ -126,42 +126,42 @@ class _MobileDetailsState extends State<MobileDetails> {
                 child: Column(
                   children: <Widget>[
 
-                     TextOfDetails(
+                     CustomTextOfDetails(
                       text1:  "التقريب : ",
                       text2: "يقرب حتي 100 مرة أثناء التصوير بالكاميرا الزوم",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:  "دعم التصوير : ",
                       text2: "يدعم تصوير الفيديوهات بجودة ال 8K",
                       containerColor: kcontainerColor2,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:  "الذاكرة العشوائية : ",
                       text2: "كبيرة جدا تعطيك مرونه في فتح الكثير من التطبيقات ولكن فعليا هل سنحتاج إلي كل هذه المساحة بالاخص مع قدومها من نوع LPDDR5",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:  "الكاميرا  : ",
                       text2:  "كامبرا قوية جدا قادرة علي المنافسة وكاد تكون الأفضل في 2020 ... مع كاميرا سيلفي مميزة 40 ميجا بيكسل",
                       containerColor: kcontainerColor2,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:  "الشاشة  : ",
                       text2:  "ممتازة وكبيرة و بدقة عالية ومعدل تحديث 120 هرتز",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:   "البطارية : ",
                       text2: "كبيرة جدا والتي يتم توفيرها لأول مرة يهذه السعة في سلسلة ال S",
                       containerColor: kcontainerColor2,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:   "الشحن : ",
                       text2: "يدعم الشحن السريع بقوة 45 واط حيث ستتمكن من شحن هاتفك من 0% إلي 100% خلال 74 دقيقة بالكامل",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:   "أداء المعالج : ",
                       text2: "قوي و يمتاز بأنه قوي من حيث الأداء",
                       containerColor: kcontainerColor2,
@@ -184,42 +184,42 @@ class _MobileDetailsState extends State<MobileDetails> {
                 padding: EdgeInsets.all(10),
                 child: Column(
                   children: <Widget>[
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:  "السعر : ",
                       text2: "مرتفع وحجمة ثقيل",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1: "المنافذ : ",
                       text2: "لا يوجد منفذ 3.5 ملم",
                       containerColor: kcontainerColor2,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1: "لمبة إشعارات : ",
                       text2: "لا توجد به لمبه إشعارات",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1: "الشبكة : ",
                       text2: "لا توجد به لمبه إشعارات",
                       containerColor: kcontainerColor2,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1: "الكاميرا : ",
                       text2:  "بروز كبير في الشكل",
                       containerColor: kcontainerColor1,
                     ),
-                   TextOfDetails(
+                   CustomTextOfDetails(
                       text1: "الألوان : ",
                       text2:  "محددة",
                       containerColor: kcontainerColor2,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1: "الشرايح : ",
                       text2:   "لا يمكن استخدام شريحتين و كارت ميموري في نفس الوقت",
                       containerColor: kcontainerColor1,
                     ),
-                    TextOfDetails(
+                    CustomTextOfDetails(
                       text1:"الشاشة : ",
                       text2:   "كبيرة جدا في اليد وستحتاج إلي بعض الوقت للاعتياد عليها",
                       containerColor: kcontainerColor2,
@@ -235,8 +235,8 @@ class _MobileDetailsState extends State<MobileDetails> {
   }
 }
 
-class TextOfDetails extends StatelessWidget {
-  const TextOfDetails(
+class CustomTextOfDetails extends StatelessWidget {
+  const CustomTextOfDetails(
       {this.containerColor, this.text1, this.text2});
   final Color containerColor;
   final String text1;
