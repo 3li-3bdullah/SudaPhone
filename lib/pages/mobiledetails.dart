96e5b6764bb1d4aa13010fd659a8c7ef33b6ec10
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-10 18:37:17 +0200

    Some edited

--- lib/pages/mobiledetails.dart
+++ lib/pages/mobiledetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../widgets/custom_textof_details.dart';
 import '../constants.dart';
 
 class MobileDetails extends StatefulWidget {
@@ -235,22 +236,4 @@ class _MobileDetailsState extends State<MobileDetails> {
   }
 }
 
-class CustomTextOfDetails extends StatelessWidget {
-  const CustomTextOfDetails(
-      {this.containerColor, this.text1, this.text2});
-  final Color containerColor;
-  final String text1;
-  final String text2;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.all(10),
-        color: containerColor,
-        child: RichText(
-            text: TextSpan(style: TextStyle(fontSize: 18), children: <TextSpan>[
-          TextSpan(text: text1, style: TextStyle(color: Colors.black)),
-          TextSpan(text: text2, style: TextStyle(color: Colors.white)),
-        ])));
-  }
-}
+
