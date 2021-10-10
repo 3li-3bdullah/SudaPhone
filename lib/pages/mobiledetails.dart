0201487ea2bc325591a083e047814ad1d24b9485
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-10 16:23:27 +0200

    Added some constructor

--- lib/pages/mobiledetails.dart
+++ lib/pages/mobiledetails.dart
@@ -507,6 +507,28 @@ class _MobileDetailsState extends State<MobileDetails> {
 }
 
 class TextOfDetails extends StatelessWidget {
+  const TextOfDetails(
+      {this.containerColor, this.text1, this.text2, this.color1, this.color2});
+  final Color containerColor;
+  final String text1;
+  final String text2;
+  final Color color1;
+  final Color color2;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(10),
+        color: Color(0xff08535d),
+        child: RichText(
+            text: TextSpan(style: TextStyle(fontSize: 18), children: <TextSpan>[
+          TextSpan(text: text1, style: TextStyle(color: color1)),
+          TextSpan(text: text2, style: TextStyle(color: color2)),
+        ])));
+  }
+}
+/* 
+class TextOfDetails extends StatelessWidget {
   const TextOfDetails({
      this.containerColor
   });
@@ -519,11 +541,12 @@ class TextOfDetails extends StatelessWidget {
         color: Colors.white,
         child: RichText(
             text: TextSpan(
-                style: TextStyle(fontSize: 18, fontFamily: 'Cairo'),
+                style: TextStyle(fontSize: 18),
                 children: <TextSpan>[
               TextSpan(
-                  text: "الموديل :", style: TextStyle(color: Colors.black)),
-              TextSpan(text: "S20 Ultra", style: TextStyle(color: Colors.blue)),
+                  text: "الموديل :", style: TextStyle(color: Colors.black,fontSize: 18)),
+              TextSpan(text: "S20 Ultra", style: TextStyle(fontSize: 18 ,color: Colors.blue)),
             ])));
   }
 }
+*/
