a248d83555712413238735402c589047f2925628
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-11 23:23:47 +0200

    Created new file and added the class

---
+++ lib/widgets/custom_text.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText({
+  required  this.text,
+  required  this.textAlign,
+  required  this.color,
+  required  this.fontSize,
+  required  this.fontWeight,
+  });
+  final String text;
+  final TextAlign textAlign;
+  final Color color;
+  final double fontSize;
+  final FontWeight fontWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textAlign,
+      style:
+          TextStyle(color: color, fontSize: fontSize, fontWeight: fontWeight),
+    );
+  }
+}
