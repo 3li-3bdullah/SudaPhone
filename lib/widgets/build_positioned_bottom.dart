d9217ed9e7700ad8badb1ae01087bac2498e7f68
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-14 14:52:05 +0200

    Created new file and added the class

---
+++ lib/widgets/build_positioned_bottom.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class BuildPositionedBottom extends StatelessWidget {
+  const BuildPositionedBottom({
+    
+    @required this.mdw,
+    @required this.showsignin,
+  }) ;
+
+  final double mdw;
+  final bool showsignin;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      //top : 300
+      top: mdw * 1.2, // this will go down
+      right: mdw / 1.5,
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        height: mdw,
+        width: mdw,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(mdw),
+         color: showsignin ? Colors.blue.withOpacity(0.3) : Colors.red.withOpacity(0.3)),
+      ),
+    );
+  }
+}
