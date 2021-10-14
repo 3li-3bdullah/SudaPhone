38d1a0d0e76980614d30c5b5bbd212069c0afbe0
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-14 14:58:42 +0200

    Extract two widgets and used it

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -506,37 +506,6 @@ class LogInState extends State<LogIn> {
 
 }
 
-class BuildPositionedTop extends StatelessWidget {
-  const BuildPositionedTop({
-    
-    @required this.mdw,
-    @required this.showsignin,
-  }) ;
-
-  final double mdw;
-  final bool showsignin;
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-        child: Transform.scale(
-      scale: 1.3,
-      child: Transform.translate(
-        offset: Offset(0, -mdw / 1.7),
-
-        /// the offset takes tow value hori and vertical hori will push it right with positive number and top with
-        /// naigtive number and to bottom with positive number
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 500),
-          height: mdw,
-          width: mdw,
-          decoration: BoxDecoration(borderRadius: BorderRadius.circular(mdw),
-           color: showsignin ? Color(0xff424242) : Colors.green),
-        ),
-      ),
-    ));
-  }
-}
 
 
 // class TextFormFieldAll extends StatelessWidget {
