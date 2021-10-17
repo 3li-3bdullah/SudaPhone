d3ff40dadf621cd7942ce8fb16cba51008665ee9
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-17 22:49:21 +0200

    Creted new class and initialize the getxcontroller

---
+++ lib/services/auth_view_model.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+class AuthViewModel extends GetxController{
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+  @override
+  void onReady() {
+    // TODO: implement onReady
+    super.onReady();
+  }
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    super.onClose();
+  }
+}
