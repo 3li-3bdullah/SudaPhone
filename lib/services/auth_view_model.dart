866833211f379611ddc09f4a6fd86457fbf0488e
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-18 21:12:12 +0200

    Declare three instance variables

--- lib/services/auth_view_model.dart
+++ lib/services/auth_view_model.dart
@@ -1,24 +1,26 @@
 import 'package:get/get.dart';
 
-class AuthViewModel extends GetxController{
+class AuthViewModel extends GetxController {
+  
+  late String email, password, name;
 
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
   }
+
   @override
   void onReady() {
     // TODO: implement onReady
     super.onReady();
   }
+
   @override
   void onClose() {
     // TODO: implement onClose
     super.onClose();
   }
 
-  signInWithEmailAndPassword(){
-    
-  }
+  signInWithEmailAndPassword() {}
 }
