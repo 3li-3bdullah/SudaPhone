576b0e70b6ba0aa7afeb68ab4ec0ad5e8c2792f4
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-19 09:21:23 +0200

    Added four instance variables of colors

--- lib/widgets/custom_text_form_field.dart
+++ lib/widgets/custom_text_form_field.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({   required this.onSave,required this.pass,
-  required this.validator,required this.icon,required this.myhinttext}) ;
+  const CustomTextFormField(
+      {required this.onSave,
+      required this.pass,
+      required this.validator,
+      required this.icon,
+      required this.myhinttext,
+      required this.fillColor,
+      required this.colorEnabled,
+      required this.colorFocused,
+      required this.colorBorderSide});
   final Function onSave;
   final bool pass;
   final Function validator;
   final IconData icon;
   final String myhinttext;
+  final Color fillColor;
+  final Color colorEnabled;
+  final Color colorFocused;
+  final Color colorBorderSide;
   @override
   Widget build(BuildContext context) {
     return TextFormField(
@@ -15,17 +27,26 @@ class CustomTextFormField extends StatelessWidget {
       onSaved: (value) {
         onSave();
       },
-     
+
       obscureText: pass,
-      validator: (val) {
+      validator: (value) {
+        validator();
       },
-      decoration: InputDecoration(contentPadding: EdgeInsets.all(4), hintText: myhinttext,
-       prefixIcon: Icon(icon, color: Colors.green), filled: true, fillColor: Color(0xffe0e0e0),
-        enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xffbdbdbd), 
-        style: BorderStyle.solid, width: 1)), focusedBorder: OutlineInputBorder(borderSide: BorderSide(
-          color: Colors.purple, style: BorderStyle.solid, width: 1)),
-           border: OutlineInputBorder(borderSide: BorderSide(color: Color(0xffbdbdbd),
-            style: BorderStyle.solid, width: 1))),
+      decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(4),
+          hintText: myhinttext,
+          prefixIcon: Icon(icon, color: Colors.green),
+          filled: true,
+          fillColor: fillColor,
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: colorEnabled, style: BorderStyle.solid, width: 1)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: colorFocused, style: BorderStyle.solid, width: 1)),
+          border: OutlineInputBorder(
+              borderSide: BorderSide(
+                  color: colorBorderSide, style: BorderStyle.solid, width: 1))),
     );
   }
 }
