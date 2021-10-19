7435c27b83a9345b8e8eaed62ef0b731f8f9e927
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-19 09:54:21 +0200

    Added two new color

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,3 +5,5 @@ const khomeBar = Color(0xff08535d);
 const kcontainerColor1 = Color(0xff9ca2ab);
 const kcontainerColor2 = Color(0xff808488);
 const kappDetails = Color(0xff555555);
+const kfillColor = Color(0xffe0e0e0);
+const kenabledColor = Color(0xffbdbdbd);
