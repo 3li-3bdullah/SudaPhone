dc3923c0b3487394deea808020ee905f30af11aa
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-21 01:38:49 +0200

    Customized size of card

--- lib/widgets/categories_card.dart
+++ lib/widgets/categories_card.dart
@@ -12,30 +12,33 @@ class CategoriesCard extends StatelessWidget {
   final Function onTap;
   @override
   Widget build(BuildContext context) {
-    return InkWell( borderRadius: BorderRadius.all(Radius.circular(30)),
-        child: Card(
-          elevation: 3,
-            child: Column(
-          children: [
-            Expanded(
-              child: Image.asset(
-                imageCat,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Container(
-               child: CustomText(
-                 text: text,
-                 fontSize: 20,
-                 fontWeight: FontWeight.normal,
-                 textAlign: TextAlign.center,
-                 color: Colors.grey.shade900,
-               ),
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: InkWell( borderRadius: BorderRadius.all(Radius.circular(30)),
+          child: Card(
+            elevation: 3,
+              child: Column(
+            children: [
+              Expanded(
+                child: Image.asset(
+                  imageCat,
+                  fit: BoxFit.cover,
                 ),
-          ],
-        )),
-        onTap: () {
-          onTap();
-        });
+              ),
+              Container(
+                 child: CustomText(
+                   text: text,
+                   fontSize: 20,
+                   fontWeight: FontWeight.normal,
+                   textAlign: TextAlign.center,
+                   color: Colors.grey.shade900,
+                 ),
+                  ),
+            ],
+          )),
+          onTap: () {
+            onTap();
+          }),
+    );
   }
 }
