e63daf9293c956202465cb7e79d143ef74f01ade
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-20 11:03:08 +0200

    Added two new cate

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -29,8 +29,7 @@ class _CategoriesState extends State<Categories> {
               body: GridView(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2),
-                children: <Widget>[
-                  //Start cat one
+                children: [
                   CategoriesCard(
                     imageCat: "example/images/category/samsung.png",
                     text: "Samsung",
@@ -38,63 +37,51 @@ class _CategoriesState extends State<Categories> {
                       Get.to(Samsung());
                     },
                   ),
-                  //End cat one
-                  //----------------------
-                  //Start cat two
                   CategoriesCard(
                     imageCat: "example/images/category/huawei.png",
                     text: "Huawei",
                     onTap: () {},
                   ),
-                  //End cat two
-                  //----------------------
-                  //Start cat three
                   CategoriesCard(
                     imageCat: "example/images/category/apple.jpg",
                     text: "Apple",
                     onTap: () {},
                   ),
-
-                  //End cat three
-                  //----------------------
-                  //Start cat four
                   CategoriesCard(
                     imageCat: "example/images/category/oppo.png",
                     text: "Oppo",
                     onTap: () {},
                   ),
-                  //End cat four
-                  //----------------------
-                  //Start cat five
                   CategoriesCard(
                     imageCat: "example/images/category/xiaomi.png",
                     text: "Xiaomi",
                     onTap: () {},
                   ),
-                  //End cat five
-                  //----------------------
-                  //Start car six
                   CategoriesCard(
                     imageCat: "example/images/category/lenovo.png",
                     text: "Lenovo",
                     onTap: () {},
                   ),
-                  //End cat six
-                  //----------------------
-                  //Start cat seven
                   CategoriesCard(
                     imageCat: "example/images/category/realme.png",
                     text: "Realme",
                     onTap: () {},
                   ),
-                  //End cat eight
-                  //----------------------
                   CategoriesCard(
                     imageCat: "example/images/category/tecno.png",
                     text: "Tecno",
                     onTap: () {},
                   ),
-                  //End cat eight
+                  CategoriesCard(
+                    imageCat: "example/images/category/vivo.png",
+                    text: "Vivo",
+                    onTap: () {},
+                  ),
+                  CategoriesCard(
+                    imageCat: "example/images/category/nokia.png",
+                    text: "Nokia",
+                    onTap: () {},
+                  ),
                 ],
               ),
             )));
