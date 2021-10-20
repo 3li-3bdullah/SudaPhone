9cf35ca9f311248b1cf55b387a3e526d99505795
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-21 01:34:43 +0200

    Edited color and size

--- lib/pages/categories.dart
+++ lib/pages/categories.dart
@@ -28,63 +28,68 @@ class _CategoriesState extends State<Categories> {
                 centerTitle: true,
               ),
               drawer: MyDrawer(),
-              body: GridView(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                children: [
-                  CategoriesCard(
-                    imageCat: "example/images/category/samsung.png",
-                    text: "Samsung",
-                    onTap: () {
-                      Get.to(Samsung());
-                    },
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/huawei.png",
-                    text: "Huawei",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/apple.jpg",
-                    text: "Apple",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/oppo.png",
-                    text: "Oppo",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/xiaomi.png",
-                    text: "Xiaomi",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/lenovo.png",
-                    text: "Lenovo",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/realme.png",
-                    text: "Realme",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/tecno.png",
-                    text: "Tecno",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/vivo.jpg",
-                    text: "Vivo",
-                    onTap: () {},
-                  ),
-                  CategoriesCard(
-                    imageCat: "example/images/category/nokia.png",
-                    text: "Nokia",
-                    onTap: () {},
-                  ),
-                ],
+              body: Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: kbackGround,padding: EdgeInsets.all(10),
+                child: GridView(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  children: [
+                    CategoriesCard(
+                      imageCat: "example/images/category/samsung.png",
+                      text: "Samsung",
+                      onTap: () {
+                        Get.to(Samsung());
+                      },
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/huawei.png",
+                      text: "Huawei",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/apple.jpg",
+                      text: "Apple",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/oppo.png",
+                      text: "Oppo",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/xiaomi.png",
+                      text: "Xiaomi",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/lenovo.png",
+                      text: "Lenovo",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/realme.png",
+                      text: "Realme",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/tecno.png",
+                      text: "Tecno",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/vivo.jpg",
+                      text: "Vivo",
+                      onTap: () {},
+                    ),
+                    CategoriesCard(
+                      imageCat: "example/images/category/nokia.png",
+                      text: "Nokia",
+                      onTap: () {},
+                    ),
+                  ],
+                ),
               ),
             )));
   }
