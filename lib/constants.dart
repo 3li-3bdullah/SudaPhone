2576199b2d6a3ca767679455d9944832605746a8
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-25 23:35:31 +0200

    Changed the color

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const kbackGround = Color(0xff80827d);
+const kbackGround = Color(0xff50827d);
 const kiconRegister = Color(0xff99bcc4);
 const kprimaryColor = Color(0xff08535d);
 const kcontainerColor1 = Color(0xff9ca2ab);
