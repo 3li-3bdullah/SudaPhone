f7bc73e6047f2bbd2e12a485d94933403e664360
Ali Abullah <PROGRMMER7@gmail.com>
2021-10-25 23:26:33 +0200

    Added Suitable size

--- lib/pages/screen.dart
+++ lib/pages/screen.dart
@@ -38,52 +38,53 @@ class ScreenState extends State<Screen> {
   Widget buildImage(String imagesCarousel, int index) => Container(
       // margin: EdgeInsets.symmetric(horizontal: 2),
       color: Colors.grey,
-      width: double.infinity,
+      width: MediaQuery.of(context).size.width,
       child: Image.asset(
         imagesCarousel,
-        fit: BoxFit.cover,width: double.infinity,
+        fit: BoxFit.cover,
+        width: MediaQuery.of(context).size.width,
       ));
-
   Widget buildIndicator() => Container(
-          child: SmoothPageIndicator(
-                  controller: controller,
-                  count: imagesCarousel.length,
-                  effect: CustomizableEffect(
-                    activeDotDecoration: DotDecoration(
-                      width: 32,
-                      height: 12,
-                      color: kprimaryColor,
-                      rotationAngle: 180,
-                      verticalOffset: -10,
-                      borderRadius: BorderRadius.circular(24),
-                      dotBorder: DotBorder(
-                        padding: 2,
-                        width: 2,
-                        color: kprimaryColor,
-                      ),
-                    ),
-                    dotDecoration: DotDecoration(
-                      width: 24,
-                      height: 12,
-                      color: Colors.grey,
-                      dotBorder: DotBorder(
-                        padding: 2,
-                        width: 2,
-                        color: Colors.grey,
-                      ),
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(2),
-                          topRight: Radius.circular(16),
-                          bottomLeft: Radius.circular(16),
-                          bottomRight: Radius.circular(2)),
-                      //borderRadius: BorderRadius.circular(16),
-                      verticalOffset: 0,
-                    ),
-                    spacing: 6.0,
-                    // activeColorOverride: (i) => colors[i],
-                    inActiveColorOverride: (i) => colors[i],
-                  ),
-                ),);
+        child: SmoothPageIndicator(
+          controller: controller,
+          count: imagesCarousel.length,
+          effect: CustomizableEffect(
+            activeDotDecoration: DotDecoration(
+              width: 32,
+              height: 12,
+              color: kprimaryColor,
+              rotationAngle: 180,
+              verticalOffset: -10,
+              borderRadius: BorderRadius.circular(24),
+              dotBorder: DotBorder(
+                padding: 2,
+                width: 2,
+                color: kprimaryColor,
+              ),
+            ),
+            dotDecoration: DotDecoration(
+              width: 24,
+              height: 12,
+              color: Colors.grey,
+              dotBorder: DotBorder(
+                padding: 2,
+                width: 2,
+                color: Colors.grey,
+              ),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(2),
+                  topRight: Radius.circular(16),
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(2)),
+              //borderRadius: BorderRadius.circular(16),
+              verticalOffset: 0,
+            ),
+            spacing: 6.0,
+            // activeColorOverride: (i) => colors[i],
+            inActiveColorOverride: (i) => colors[i],
+          ),
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -117,12 +118,11 @@ class ScreenState extends State<Screen> {
                 child: GridTile(
                   child: PageView.builder(
                     controller: controller,
-                   itemCount: imagesCarousel.length,
-                    itemBuilder: (context, index ) {
+                    itemCount: imagesCarousel.length,
+                    itemBuilder: (context, index) {
                       String imagesCar = imagesCarousel[index];
                       return buildImage(imagesCar, index);
-                    
-                  },
+                    },
                     // itemCount: imagesCarousel.length,
                     // itemBuilder: (context, index, realIndex) {
                     //   String imagesCar = imagesCarousel[index];
@@ -144,7 +144,7 @@ class ScreenState extends State<Screen> {
               ),
               Container(
                 padding: EdgeInsets.all(10),
-                child:CustomText(
+                child: CustomText(
                   text: "الأقسام",
                   textAlign: TextAlign.right,
                   color: Colors.grey.shade800,
@@ -158,54 +158,54 @@ class ScreenState extends State<Screen> {
                   height: 120,
                   child: ListView(scrollDirection: Axis.horizontal, children: [
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/huawei.png",
-                     text: "Huawei",
-                    onTap:  () {},
+                      imageLogo: "example/images/logo/huawei.png",
+                      text: "Huawei",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/iphone.jpg",
-                     text: "Apple",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/iphone.jpg",
+                      text: "Apple",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/realme.png",
-                     text: "Realme",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/realme.png",
+                      text: "Realme",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/lenovo.png",
-                     text: "Lenovo",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/lenovo.png",
+                      text: "Lenovo",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/vivo.png",
-                     text: "Vivo",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/vivo.png",
+                      text: "Vivo",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/samsung.jpg",
-                      text:"Samsung",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/samsung.jpg",
+                      text: "Samsung",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/xiaomi.png",
-                     text: "Xiaomi",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/xiaomi.png",
+                      text: "Xiaomi",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                      imageLogo:"example/images/logo/oppo.jpg",
-                     text: "Oppo",
-                    onTap:  () {},
+                      imageLogo: "example/images/logo/oppo.jpg",
+                      text: "Oppo",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/tecno.png",
-                     text: "Tecno",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/tecno.png",
+                      text: "Tecno",
+                      onTap: () {},
                     ),
                     CategoriesLogo(
-                     imageLogo: "example/images/logo/nokia.jpg",
-                     text: "Nokia",
-                     onTap: () {},
+                      imageLogo: "example/images/logo/nokia.jpg",
+                      text: "Nokia",
+                      onTap: () {},
                     ),
                   ])),
               // End The Long Container....
