b4b323682bc1d67bc37700f38c7c973042906b93
Ali Abullah <PROGRMMER7@gmail.com>
2021-11-18 17:20:43 +0200

    Some editing

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,6 +5,5 @@ const kiconRegister = Color(0xff99bcc4);
 const kprimaryColor = Color(0xff08535d);
 const kcontainerColor1 = Color(0xff9ca2ab);
 const kcontainerColor2 = Color(0xff808488);
-const kappDetails = Color(0xff555555);
 const kfillColor = Color(0xffe0e0e0);
 const kenabledColor = Color(0xffbdbdbd);
